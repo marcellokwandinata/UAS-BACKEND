3b19394a3f4847cca73b2f69f20db90b8fe48eeb
clajulietta <clarissajulieta07@gmail.com>
2026-05-29 04:24:45 +0700

    mengubah tampilan view topup agar sama dgn view history

--- resources/views/list_topup.blade.php
+++ resources/views/list_topup.blade.php
@@ -1,40 +1,42 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Topup Data</title>
+    <title>Topup List</title>
 
     <style>
+        body {
+            font-family: Arial, sans-serif;
+            min-height: 100vh;
+            padding: 3vh 3vw;
+        }
+
         table {
             border-collapse: collapse;
-            width: 70%;
+            width: 70vw;
         }
 
         th, td {
             border: 1px solid black;
-            padding: 10px;
+            padding: 1.5vh 1vw;
             text-align: center;
         }
 
         th {
             background-color: #ddd;
         }
+
+        a {
+            text-decoration: none;
+            padding: 1vh 1vw;
+            background: black;
+            color: white;
+            border-radius: 5px;
+        }
     </style>
 </head>
 <body>
 
 <h1>Topup Data</h1>
-
-@if(session('success'))
-    <p style="color: green;">
-        {{ session('success') }}
-    </p>
-@endif
-
-@if(session('error'))
-    <p style="color: red;">
-        {{ session('error') }}
-    </p>
-@endif
 
 <a href="/">Home</a>
 
@@ -44,30 +46,38 @@
 
 <br><br>
 
-<form action="/topups/" method="GET">
-    <input type="number" name="id" placeholder="Cari ID">
-</form>
+@if(session('success'))
+    <p style="color: green;">{{ session('success') }}</p>
+@endif
 
-<br>
+@if(session('error'))
+    <p style="color: red;">{{ session('error') }}</p>
+@endif
 
-<table border="1" cellpadding="8">
+<table>
     <tr>
         <th>ID</th>
-        <th>Email</th>
-        <th>Amount</th>
-        <th>Aksi</th>
+        <th>Payment Method</th>
+        <th>Nominal</th>
+        <th>Status</th>
+        <th>Action</th>
     </tr>
 
     @foreach($topups as $topup)
     <tr>
         <td>{{ $topup->id }}</td>
-        <td>{{ $topup->email }}</td>
-        <td>{{ $topup->amount }}</td>
+        <td>{{ $topup->payment_method }}</td>
+        <td>{{ $topup->nominal }}</td>
+        <td>{{ $topup->status }}</td>
         <td>
-            <a href="/topup/delete/{{ $topup->delete }}">Hapus</a>
+            <a href="/topups/delete/{{ $topup->id }}">
+                Hapus
+            </a>
         </td>
     </tr>
     @endforeach
+
 </table>
+
 </body>
 </html>
