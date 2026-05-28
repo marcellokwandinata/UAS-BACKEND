051c3133fe3d4e6b3c22701d2b9c0732b4673902
clajulietta <clarissajulieta07@gmail.com>
2026-05-29 01:47:04 +0700

    menambahkan form untuk post history di view

---
+++ resources/views/create_history.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Create History</title>
+</head>
+<body>
+
+    <h1>Create History</h1>
+
+    <form method="POST" action="/histories">
+        @csrf
+
+        <label>Title:</label>
+        <br>
+        <input type="text" name="title">
+        <br><br>
+
+        <label>Description:</label>
+        <br>
+        <input type="text" name="description">
+        <br><br>
+
+        <label>Amount:</label>
+        <br>
+        <input type="number" name="amount">
+        <br><br>
+
+        <button type="submit">
+            Submit
+        </button>
+    </form>
+
+</body>
+</html>
