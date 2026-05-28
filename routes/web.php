192152a6a536a091d03bc153dc8ea065ce6f773d
clajulietta <clarissajulieta07@gmail.com>
2026-05-29 01:51:07 +0700

    menambahkan routes form history dan topup

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,14 @@ Route::get('/histories', [HistoryController::class, 'index']);
 Route::get('/histories/{id}', [HistoryController::class, 'show']);
 Route::post('/histories', [HistoryController::class, 'store']);
 Route::delete('/histories/{id}', [HistoryController::class, 'destroy']);
+Route::get('/histories/create', function () {
+    return view('create_history');
+});
 
 // topups
 Route::get('/topups', [TopupController::class, 'index']);
 Route::get('/topups/{id}', [TopupController::class, 'show']);
 Route::post('/topups', [TopupController::class, 'store']);
+Route::get('/topups/create', function () {
+    return view('create_topup');
+});
