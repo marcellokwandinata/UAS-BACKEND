bcbd1e515b057f5d49191d5e3b003d7818994106
clajulietta <clarissajulieta07@gmail.com>
2026-05-29 01:56:24 +0700

    update isi dari web.php

--- routes/web.php
+++ routes/web.php
@@ -10,17 +10,18 @@ Route::get('/', function () {
 
 // histories
 Route::get('/histories', [HistoryController::class, 'index']);
-Route::get('/histories/{id}', [HistoryController::class, 'show']);
-Route::post('/histories', [HistoryController::class, 'store']);
-Route::delete('/histories/{id}', [HistoryController::class, 'destroy']);
 Route::get('/histories/create', function () {
     return view('create_history');
 });
+Route::get('/histories/{id}', [HistoryController::class, 'show']);
+Route::post('/histories', [HistoryController::class, 'store']);
+Route::delete('/histories/{id}', [HistoryController::class, 'destroy']);
+
 
 // topups
 Route::get('/topups', [TopupController::class, 'index']);
-Route::get('/topups/{id}', [TopupController::class, 'show']);
-Route::post('/topups', [TopupController::class, 'store']);
 Route::get('/topups/create', function () {
     return view('create_topup');
 });
+Route::get('/topups/{id}', [TopupController::class, 'show']);
+Route::post('/topups', [TopupController::class, 'store']);
