a7b3a5b026e40cf408696a12ec14556d8c90c0b4
clajulietta <clarissajulieta07@gmail.com>
2026-05-29 00:56:30 +0700

    menambahkan router untuk histories dan topup

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HistoryController;
+use App\Http\Controllers\TopupController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// histories
+Route::get('/histories', [HistoryController::class, 'index']);
+Route::get('/histories/{id}', [HistoryController::class, 'show']);
+Route::delete('/histories/{id}', [HistoryController::class, 'destroy']);
+
+// topups
+Route::get('/topups', [TopupController::class, 'index']);
+Route::get('/topups/{id}', [TopupController::class, 'show']);
