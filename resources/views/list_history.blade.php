f523387255ad4f075ed90adb1bc0ebfcde83ecc1
clajulietta <clarissajulieta07@gmail.com>
2026-05-29 03:52:48 +0700

    menambahkan keterangan jika data berhasil di hapus, atau id tidak ditemukan jika dicari

--- resources/views/list_history.blade.php
+++ resources/views/list_history.blade.php
@@ -7,6 +7,18 @@
 
 <h1>History Data</h1>
 
+@if(session('success'))
+    <p style="color: green;">
+        {{ session('success') }}
+    </p>
+@endif
+
+@if(session('error'))
+    <p style="color: red;">
+        {{ session('error') }}
+    </p>
+@endif
+
 <a href="/">Home</a>
 
 <br><br>
