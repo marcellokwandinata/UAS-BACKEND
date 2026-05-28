fd64e9931c57f00dca040e9c80bbf45234d5c0b9
WilbertAllanMuljadi <wilbert.535250053@stu.untar.ac.id>
2026-05-28 14:35:27 +0700

    routes acc

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\AccountController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/accounts');
 });
+
+Route::resource('posts', PostController::class);
+
+Route::resource('accounts', AccountController::class);
