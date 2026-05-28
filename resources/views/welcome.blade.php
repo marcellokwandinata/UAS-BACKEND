a32fae71a1e1efd522b60a7efd842788ad57ef38
clajulietta <clarissajulieta07@gmail.com>
2026-05-29 01:53:37 +0700

    menambahkan halaman create histories dan topups

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Laravel API</title>
+    <title>Digital Banking</title>
 
     <style>
         body {
@@ -11,6 +11,10 @@
 
         h1 {
             margin-bottom: 20px;
+        }
+
+        h2 {
+            margin-top: 30px;
         }
 
         ul {
@@ -34,11 +38,18 @@
 
 <body>
 
-    <h1>Laravel API Dashboard</h1>
+    <h1>Digital Banking</h1>
 
+    <h2>Histories</h2>
     <ul>
         <li><a href="/histories">View Histories</a></li>
+        <li><a href="/histories/create">Create History</a></li>
+    </ul>
+
+    <h2>Topups</h2>
+    <ul>
         <li><a href="/topups">View Topups</a></li>
+        <li><a href="/topups/create">Create Topup</a></li>
     </ul>
 
 </body>
