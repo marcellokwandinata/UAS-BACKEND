2e6956581b3481ca12bc7c75f36c976564edd653
clajulietta <clarissajulieta07@gmail.com>
2026-05-29 01:50:00 +0700

    menambahkan form untuk post topup di view

---
+++ resources/views/create_topup.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Create Topup</title>
+</head>
+<body>
+
+    <h1>Create Topup</h1>
+
+    <form method="POST" action="/topups">
+        @csrf
+
+        <label>Payment Method:</label>
+        <br>
+        <input type="text" name="payment_method">
+        <br><br>
+
+        <label>Nominal:</label>
+        <br>
+        <input type="number" name="nominal">
+        <br><br>
+
+        <label>Status:</label>
+        <br>
+        <input type="text" name="status">
+        <br><br>
+
+        <button type="submit">
+            Submit
+        </button>
+    </form>
+
+</body>
+</html>
