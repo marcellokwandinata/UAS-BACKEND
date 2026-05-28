3f56df46a77d2b3a56a8b7f51b606d826b52069d
clajulietta <clarissajulieta07@gmail.com>
2026-05-29 03:28:36 +0700

    mengubah codingan agar file ini khusus create saja

--- resources/views/create_history.blade.php
+++ resources/views/create_history.blade.php
@@ -1,61 +1,44 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>History List</title>
-
-    <style>
-        table {
-            border-collapse: collapse;
-            width: 70%;
-        }
-
-        th, td {
-            border: 1px solid black;
-            padding: 8px 12px;
-            text-align: center;
-        }
-
-        th {
-            background-color: #e5e5e5;
-            font-weight: bold;
-        }
-
-        a {
-            color: blue;
-        }
-    </style>
+    <title>Create History</title>
 </head>
 <body>
 
-<h1>History Data</h1>
+<h1>Tambah History</h1>
 
-<a href="/">Kembali ke Home</a>
-<br><br>
+<form action="/histories" method="POST">
+    @csrf
 
-<table>
-    <tr>
-        <th>ID</th>
-        <th>Title</th>
-        <th>Description</th>
-        <th>Amount</th>
-        <th>Action</th>
-    </tr>
+    <div>
+        <label>Title</label>
+        <input type="text" name="title">
+    </div>
 
-    @foreach($histories as $history)
-    <tr>
-        <td>{{ $history->id }}</td>
-        <td>{{ $history->title }}</td>
-        <td>{{ $history->description }}</td>
-        <td>{{ $history->amount }}</td>
-        <td>
-            <a href="/histories/delete/{{ $history->id }}">
-                Hapus
-            </a>
-        </td>
-    </tr>
-    @endforeach
+    <br>
 
-</table>
+    <div>
+        <label>Description</label>
+        <input type="text" name="description">
+    </div>
+
+    <br>
+
+    <div>
+        <label>Amount</label>
+        <input type="number" name="amount">
+    </div>
+
+    <br>
+
+    <button type="submit">
+        Simpan
+    </button>
+</form>
+
+<br>
+
+<a href="/histories">Kembali ke List</a>
 
 </body>
 </html>
