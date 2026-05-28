0456c930cbdc01334d42823982e4ef815d207b59
clajulietta <clarissajulieta07@gmail.com>
2026-05-29 02:37:24 +0700

    membuat file baru untuk tampilan list dari topup

---
+++ resources/views/list_topup.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Topup List</title>
+
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 70%;
+        }
+
+        th, td {
+            border: 1px solid black;
+            padding: 10px;
+            text-align: center;
+        }
+
+        th {
+            background-color: #ddd;
+        }
+    </style>
+</head>
+<body>
+
+<h1>Data Topup</h1>
+
+<a href="/topups/create">Tambah Topup</a>
+
+<br><br>
+
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Payment Method</th>
+        <th>Nominal</th>
+        <th>Status</th>
+    </tr>
+
+    @foreach($topups as $topup)
+    <tr>
+        <td>{{ $topup->id }}</td>
+        <td>{{ $topup->payment_method }}</td>
+        <td>{{ $topup->nominal }}</td>
+        <td>{{ $topup->status }}</td>
+    </tr>
+    @endforeach
+
+</table>
+
+</body>
+</html>
