2d342fd1221773c6c281da0693cf46dc113c741f
clajulietta <clarissajulieta07@gmail.com>
2026-05-29 00:53:46 +0700

    menambahkan topup controller

---
+++ app/Http/Controllers/TopupController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Topup;
+use Illuminate\Http\Request;
+
+class TopupController extends Controller
+{
+    // GET /topups
+    public function index()
+    {
+        $topups = Topup::all();
+
+        return response()->json([
+            'success' => true,
+            'data' => $topups
+        ]);
+    }
+
+    // GET /topups/{id}
+    public function show($id)
+    {
+        $topup = Topup::find($id);
+
+        if (!$topup) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Topup not found'
+            ], 404);
+        }
+
+        return response()->json([
+            'success' => true,
+            'data' => $topup
+        ]);
+    }
+}
