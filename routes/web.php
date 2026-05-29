051fbae9dff2367152de8111d3c4aacdd0f4c413
clajulietta <clarissajulieta07@gmail.com>
2026-05-30 02:14:58 +0700

    tambahin routes utk reset balances

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/reset-balance', function () {
+    session()->forget('balance'); // hapus session lama
+    session(['balance' => 5000000]); // set ulang
+
+    return "Balance reset ke 5.000.000";
+});
+
 // topups
 Route::get('/topups/create', function () {
     return view('create_topup');
 });
 Route::get('/topups', [TopupController::class, 'index']);
 Route::get('/topups/{id}', [TopupController::class, 'show']);
+Route::post('/topups', [TopupController::class, 'store']);
 
 // histories
 Route::get('/histories', [HistoryController::class, 'index']);
