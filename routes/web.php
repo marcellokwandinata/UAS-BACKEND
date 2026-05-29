77c1e46914b073fcc37d90c06d33d180c3475da2
clajulietta <clarissajulieta07@gmail.com>
2026-05-30 01:04:22 +0700

    mengubah route

--- routes/web.php
+++ routes/web.php
@@ -8,22 +8,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// histories
-Route::get('/histories', [HistoryController::class, 'index']);
-Route::get('/histories/create', function () {
-    return view('create_history');
-});
-Route::get('/histories/delete/{id}', [HistoryController::class, 'destroy']);
-Route::get('/histories/{id}', [HistoryController::class, 'show']);
-Route::post('/histories', [HistoryController::class, 'store']);
-
-
-
 // topups
 Route::get('/topups', [TopupController::class, 'index']);
 Route::get('/topups/create', function () {
     return view('create_topup');
 });
-Route::get('/topups/delete/{id}', [TopupController::class, 'destroy']);
-Route::get('/topups/{id}', [TopupController::class, 'show']);
 Route::post('/topups', [TopupController::class, 'store']);
+Route::get('/topups/{id}', [TopupController::class, 'show']);
+
+// histories
+Route::get('/histories', [HistoryController::class, 'index']);
+Route::get('/histories/{id}', [HistoryController::class, 'show']);
+
