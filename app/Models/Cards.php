af6adcd554a424fce4416956143f4a245e3c9447
CherylNataniaTan <cnataniatan@gmail.com>
2026-05-30 21:36:02 +0700

    mengisi file model cards

--- app/Models/Cards.php
+++ app/Models/Cards.php
@@ -1,10 +1,26 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Str;
 class Cards extends Model
 {
-    //
+    protected $table = 'cards';
+    public $incrementing = false;
+    protected $keyType = 'string';
+    protected $fillable = [
+        'user_id',
+        'card_number',
+        'card_type',
+        'expired_at',
+        'status',
+        'created_at',
+        'updated_at'
+    ];
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            $model->id = Str::uuid();
+        });
+    }
 }
