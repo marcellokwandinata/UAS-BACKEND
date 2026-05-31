a6fb9a67ac594e5bd89a85155268c9830fe9d588
clajulietta <clarissajulieta07@gmail.com>
2026-05-31 21:53:40 +0700

    benerin codingan agar dapat mencari transaksi tertentu melalui id

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -11,7 +11,14 @@ class HistoryController extends Controller
     // GET /histories
     public function index()
     {
-        $histories = History::orderBy('created_at', 'desc')->get();
+        if ($request->id) {
+            $histories = History::where('id', $request->id)
+                ->orderBy('created_at', 'desc')
+                ->get();
+        } else {
+            $histories = History::orderBy('created_at', 'desc')
+                ->get();
+        }
 
         return view('list_history', compact('histories'));
     }
@@ -30,46 +37,4 @@ class HistoryController extends Controller
     }
 }
 
-    // // GET /histories/{id}
-    // public function show($id)
-    // {
-    //     $history = History::find($id);
-
-    //      if (!$history) {
-    //         return redirect('/histories')
-    //             ->with('error', 'ID tidak ditemukan!');
-    //     }
-
-    //     return view('list_history', [
-    //         'histories' => [$history]
-    //     ]);
-    // }
-
-    // // POST /histories
-    // public function store(Request $request)
-    // {
-    //     $history = History::create([
-    //         'title' => $request->title,
-    //         'description' => $request->description,
-    //         'amount' => $request->amount,
-    //     ]);
-
-    //     return redirect('/histories');
-    // }
-
-    // // DELETE /histories/{id}
-    // public function destroy($id)
-    // {
-    //     $history = History::find($id);
-
-    //     if ($history) {
-    //         $history->delete();
-
-    //         return redirect('/histories')
-    //             ->with('success', 'Data berhasil dihapus!');
-    //     }
-
-    //     return redirect('/histories')
-    //         ->with('error', 'Data tidak ditemukan!');
-    // }
-// }
+  
